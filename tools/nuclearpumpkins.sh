ea06a14c73c70ce0ee099a25cbc3eb320c35e24c
Antonio Lain <antlai@cafjs.com>
2013-01-13 22:02:28 -0800

    Copy secrets with tools

--- tools/nuclearpumpkins.sh
+++ tools/nuclearpumpkins.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CAFDIR=$HOME/.caf
 pushd ${DIR}
 export apps='accounts'
 
 pushd ../caf_core/public/enyo/
 ./tools/deploy.sh
+popd
+
+pushd ../caf_examples/accounts/lib
+cp $CAFDIR/rsa_priv.pem .
 popd
 
 
--- tools/pumpkins.sh
+++ tools/pumpkins.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CAFDIR=$HOME/.caf
 pushd ${DIR}
 
 export apps='website helloworld hellosharing mail moody pull mutant justonce turtles tutorial1a tutorial1b tutorial1c tutorial1d tutorial1e'
 
 pushd ../caf_core/public/enyo/
 ./tools/deploy.sh
+popd
+
+pushd ../caf_examples/turtles/lib
+CFPASS=`cat ${CAFDIR}/cfpassword`
+sed -i s/cfpassword/${CFPASS}/g framework.json
 popd
 
 
