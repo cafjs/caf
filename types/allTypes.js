d88dd6beaec9ac25295b33e4d7b811738d1fd78f
Antonio Lain <antlai@cafjs.com>
2022-02-11 13:07:46 -0800

    Add caf_delay
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- types/allTypes.js
+++ types/allTypes.js
@@ -340,3 +340,33 @@
  * @global
  * @typedef {Object.<string, Object>} ctxType
  */
+
+/**
+ * @global
+ * @typedef {Object} repeaterType
+ * @property {Number} interval Time in seconds between repetitions.
+ * @property {Number=} completed The number of times an action was executed.
+ * @property {Number=} whenLastCompleted A date in msec after 1970 UTC when
+ * this action was last executed.
+ * @property {Number=} maxTimes The maximum number of repetitions. When
+ * missing it repeats until cancelled.
+ */
+
+/**
+ * @global
+ * @typedef {Object} scheduledTaskType
+ * @property {String} id A unique identifier for this task.
+ * @property {Number} when A date in msec since 1970 UTC when the task executes.
+ * @property {function(string)} handlerF The handler to trigger the task.
+ */
+
+/**
+ * @global
+ * @typedef {Object} pendingMethodType
+ * @property {String} id A unique identifier for this method.
+ * @property {Number} when A date in msec since 1970 when the method executes.
+ * @property {String} method  The method name.
+ * @property {repeaterType|null} repeater Configuration to repeat the method
+ * call. When `null` it does not repeat.
+ * @property {Array.<jsonType>} args The method arguments.
+ */
